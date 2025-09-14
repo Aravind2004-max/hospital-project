682080aa40f5438714dcb2d0561e374e5f52d410
aravindh <aravindrajan544@gmail.com>
2025-09-14 23:51:17 +0530

    minor bug

--- backend/Server.js
+++ backend/Server.js
@@ -9,7 +9,7 @@ const app = exp();
 //middlewares
 app.use(exp.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../frontend/dist/frontend')));
+app.use(exp.static(path.join(__dirname, '../frontend/dist/frontend')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/frontend/index.html'));
