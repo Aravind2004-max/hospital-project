baf5389294b24c21c5a8f6310d6cfd7fdda642ad
aravindh <aravindrajan544@gmail.com>
2025-09-14 21:06:05 +0530

    minor modification

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 const exp = require("express");
 const mongo = require("mongoose");
-const routes = require("../backend/routes/hospitalRoutes");
+const routes = require("../hospital/backend/routes");
 const cors = require("cors");
 const port = process.env.port || 3000;
 const app = exp();
