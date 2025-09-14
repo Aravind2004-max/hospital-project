64453564c4bd335d82bd766b0a898457e9d51327
aravindh <aravindrajan544@gmail.com>
2025-09-14 22:07:07 +0530

    static paths

--- backend/Server.js
+++ backend/Server.js
@@ -8,6 +8,12 @@ const app = exp();
 //middlewares
 app.use(exp.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../frontend/dist/frontend')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/frontend/index.html'));
+});
+
 app.use("/hospital",routes);
 
 //database connection part
