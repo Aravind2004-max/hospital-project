38a5d5a6fbffb035deb3b582b1f18dc1080e4346
aravindh <aravindrajan544@gmail.com>
2025-09-15 00:40:03 +0530

    changing routes into *

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ app.use(exp.json());
 app.use(cors());
 app.use(exp.static(path.join(__dirname, '../frontend/dist/frontend')));
 
-app.get('/^\/(?!hospital).*/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/frontend/index.html'));
 });
 
